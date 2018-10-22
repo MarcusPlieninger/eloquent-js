a89eed92c9baee568f9aa3b0407075bdd35e2dd7
MarcusPlieninger <mwp2101@columbia.edu>
2018-10-22 14:02:16 -0400

    More edits

--- ch2/loopingTriangle.test.js
+++ ch2/loopingTriangle.test.js
@@ -1,10 +1,13 @@
 function loopingTriangle() {
+  let triangle = ''
   for (let line = '#'; line.length < 8; line += '#') {
-    console.log(line)
+    triangle = line + '\n'
+  return triangle
   }
 
 describe('loopingTriangle()', () => {
   test('makes triangle of 7 lines', () => {
     expect(loopingTriangle()).stringContaining('#\n##\n###\n####\n#####\n######\n#######\n')
+    })
   })
-})
+}
