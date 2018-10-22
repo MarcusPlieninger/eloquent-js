63fc77762d3ddf32c9209c8069ec748cf4ce2028
MarcusPlieninger <mwp2101.columbia.edu>
2018-10-22 12:43:02 -0400

    Rewriting as function for testing

--- ch2/loopingTriangle.test.js
+++ ch2/loopingTriangle.test.js
@@ -1,10 +1,10 @@
-function loopingTriangle(){
-    for (let line = "#"; line.length < 8; line += "#"){   
-        console.log(line);
-    }
+function loopingTriangle() {
+  for (let line = '#'; line.length < 8; line += '#') {
+    console.log(line)
+  }
 
 describe('loopingTriangle()', () => {
-    test('makes triangle of 7 lines', () => {
-        expect(loopingTriangle()).stringContaining('#\n##\n###\n####\n#####\n######\n#######\n')
-    })
+  test('makes triangle of 7 lines', () => {
+    expect(loopingTriangle()).stringContaining('#\n##\n###\n####\n#####\n######\n#######\n')
+  })
 })
