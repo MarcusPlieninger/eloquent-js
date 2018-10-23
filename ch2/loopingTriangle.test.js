df5f59cd9fdf678021b4a353070b665e21093c49
MarcusPlieninger <mwp2101@columbia.edu>
2018-10-23 07:42:20 -0400

    Built triangle as return value for testing

--- ch2/loopingTriangle.test.js
+++ ch2/loopingTriangle.test.js
@@ -1,13 +1,21 @@
 function loopingTriangle() {
   let triangle = ''
   for (let line = '#'; line.length < 8; line += '#') {
-    triangle = line + '\n'
-  return triangle
+    console.log(line)
+
+    // build a triangle as a return value for testing
+    // first line of triangle will have exactly 1 '#' and 1 newline character
+    if (line.length === 1) triangle = line + '\n'
+    // now we build on the previous triangle
+    else if (line.length < 7) triangle += line + '\n'
+    // last line of triangle will not have a newline character
+    else triangle += line
   }
+  return triangle
+}
 
 describe('loopingTriangle()', () => {
-  test('makes triangle of 7 lines', () => {
-    expect(loopingTriangle()).stringContaining('#\n##\n###\n####\n#####\n######\n#######\n')
-    })
+  test('test return value', () => {
+    expect(loopingTriangle()).toBe('#\n##\n###\n####\n#####\n######\n#######')
   })
-}
+})
