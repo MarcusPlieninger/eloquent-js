3a0149b72a53161ac485ac6a911574c779890b4a
MarcusPlieninger <mwp2101@columbia.edu>
2018-10-27 12:25:51 -0400

    Removed if statements on lines 8 and 10

--- ch2/loopingTriangle.test.js
+++ ch2/loopingTriangle.test.js
@@ -2,20 +2,14 @@ function loopingTriangle() {
   // build a triangle as a return value for testing
   let triangle = ''
   for (let line = '#'; line.length < 8; line += '#') {
-    console.log(line)
-
-    // first line of triangle will have exactly 1 '#' and 1 newline character
-    if (line.length === 1) triangle = line + '\n'
-    // now we build on the previous triangle
-    else if (line.length < 7) triangle += line + '\n'
-    // last line of triangle will not have a newline character
-    else triangle += line
+    console.log(line) 
+    triangle += line + '\n' 
   }
   return triangle
 }
 
 describe('loopingTriangle()', () => {
   test('test return value', () => {
-    expect(loopingTriangle()).toBe('#\n##\n###\n####\n#####\n######\n#######')
+    expect(loopingTriangle()).toBe('#\n##\n###\n####\n#####\n######\n#######\n')
   })
 })
