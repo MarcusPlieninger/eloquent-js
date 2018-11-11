718043968eedefadf41b719662d7109fbdea8387
MarcusPlieninger <mwp2101@columbia.edu>
2018-11-10 19:21:09 -0500

    need to fix input validation

--- ch2/triangle2.test.js
+++ ch2/triangle2.test.js
@@ -1,95 +1,105 @@
 function triangle2(size) {
-  //input validation
-  if (size < 0 || isNaN(size)) {
+  // input validation
+
+  if (!size) return 'Triangle size most be a positive integer.'
+  if (isNaN(size)) return 'C\'mon, man, just enter a number.'
+  if (size % 1 != 0) return "Triangle size cannot be floating point."
+  if (size === 0) return 'Triangle size cannot be 0.'
+  if (size < 0) return 'Triangle size must be a postiive integer.'
+
+  if (size < 0) {
     return 'Triangle size must be positive integer.'
-  } else if (size === 0) {
+  } if (!(Number.isNaN(size))) {
+    return 'C\'mon, man, just enter a number.'
+  } if (size === 0) {
     return 'Triangle size cannot be 0.'
-  } else if (size % 1 === 0) {
-    return 'Triangle size cannot be floating-point.' 
-  } else {
-  //once input is validated, loop according to input
+  } if (size % 1 !== 0) {
+    return 'Triangle size cannot be floating-point.'
+  }
+  // once input is validated, loop according to input
   let triangle = ''
   for (let line = '#'; line.length < size; line += '#') {
-    console.log(line) 
-    triangle += line + '\n' 
+    console.log(line)
+    triangle += `${line}\n`
   }
   return triangle
 }
 
-describe('Triangle()', () => {
-    test('test input validation', () => {
-      expect(triangle2(-230)).toBe('Triangle size must be positive integer.')
-      expect(triangle2('Dog')).toBe('Triangle size must be positive integer.')
-      expect(triangle2(0)).toBe('Triangle size cannot be 0.')
-      expect(triangle2(false)).toBe('Triangle size must be positive integer.')
-      expect(triangle2(2.4325).toBe('Triangle size cannot be floating-point.')
-    })
-    test('small inputs', () => {
-      expect(Triangle(3)).toBe('#\n##\n###\n')
-      expect(Triangle(5)).toBe('#\n##\n###\n####\n#####')
-    })
-    test('medium inputs', () => {
-      expect(Triangle(10)).toBe(
-        '
-        #\n
-        ##\n
-        ###\n
-        ####\n
-        #####\n
-        ######\n
-        #######\n
-        ########\n
-        #########\n')
-      expect(Triangle(50)).toBe(
-        '
-        #\n
-        ##\n
-        ###\n
-        ####\n
-        #####\n
-        ######\n
-        #######\n
-        ########\n
-        #########\n
-        ##########\n
-        ###########\n
-        ############\n
-        #############\n
-        ##############\n
-        ###############\n
-        ################\n
-        #################\n
-        ##################\n
-        ###################\n
-        ####################\n
-        #####################\n
-        ######################\n
-        #######################\n
-        ########################\n
-        #########################\n
-        ##########################\n
-        ###########################\n
-        ############################\n
-        #############################\n
-        ##############################\n
-        ###############################\n
-        ################################\n
-        #################################\n
-        ##################################\n
-        ###################################\n
-        ####################################\n
-        #####################################\n
-        ######################################\n
-        #######################################\n
-        ########################################\n
-        #########################################\n
-        ##########################################\n
-        ###########################################\n
-        ############################################\n
-        #############################################\n
-        ##############################################\n
-        ###############################################\n
-        ################################################\n
-        #################################################\n')
-    })
+describe('triangle2()', () => {
+  test('if input is not a positive integer', () => {
+    expect(triangle2(-230)).toBe('Triangle size must be a positive integer.')
+    expect(triangle2('Dog')).toBe('C\'mon, man, just enter a number.')
+    expect(triangle2(0)).toBe('Triangle size cannot be 0.')
+    expect(triangle2(false)).toBe('Triangle size must be a positive integer.')
+    expect(triangle2(2.4325)).toBe('Triangle size cannot be floating point.')
   })
+
+  test('small inputs', () => {
+    expect(triangle2(3)).toBe('#\n##\n###\n')
+    expect(triangle2(5)).toBe('#\n##\n###\n####\n#####\n')
+  })
+  test('medium inputs', () => {
+    expect(triangle2(10)).toBe(
+      '#\n'
+      + '##\n'
+      + '###\n'
+      + '####\n'
+      + '#####\n'
+      + '######\n'
+      + '#######\n'
+      + '########\n'
+      + '#########\n'
+    )
+    expect(triangle2(50)).toBe(
+      '#\n'
+      + '##\n'
+      + '###\n'
+      + '####\n'
+      + '#####\n'
+      + '######\n'
+      + '#######\n'
+      + '########\n'
+      + '#########\n'
+      + '##########\n'
+      + '###########\n'
+      + '############\n'
+      + '#############\n'
+      + '##############\n'
+      + '###############\n'
+      + '################\n'
+      + '#################\n'
+      + '##################\n'
+      + '###################\n'
+      + '####################\n'
+      + '#####################\n'
+      + '######################\n'
+      + '#######################\n'
+      + '########################\n'
+      + '#########################\n'
+      + '##########################\n'
+      + '###########################\n'
+      + '############################\n'
+      + '#############################\n'
+      + '##############################\n'
+      + '###############################\n'
+      + '################################\n'
+      + '#################################\n'
+      + '##################################\n'
+      + '###################################\n'
+      + '####################################\n'
+      + '#####################################\n'
+      + '######################################\n'
+      + '#######################################\n'
+      + '########################################\n'
+      + '#########################################\n'
+      + '##########################################\n'
+      + '###########################################\n'
+      + '############################################\n'
+      + '#############################################\n'
+      + '##############################################\n'
+      + '###############################################\n'
+      + '################################################\n'
+      + '#################################################\n'
+    )
+  })
+})
