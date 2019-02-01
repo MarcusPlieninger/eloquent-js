17c12c2b90f27e3f40fe3b5b08951896a2b68b4a
MarcusPlieninger <mwp2101.columbia.edu>
2019-02-01 15:28:41 -0500

    fixed tests

--- ch2/fizzBuzz.test.js
+++ ch2/fizzBuzz.test.js
@@ -42,22 +42,22 @@ describe('large inputs', () => {
     expect(fizzBuzz(50)).toEqual(
       [1, 2, 'Fizz', 4, 'Buzz', 'Fizz', 7, 8, 'Fizz', 'Buzz',
         11, 'Fizz', 13, 14, 'Fizz', 16, 17, 'Fizz', 19, 'Buzz',
-        'Fizz', 22, 23, 'Fizz', 'Buzz', 26, 27, 28, 29, 'Fizz',
+        'Fizz', 22, 23, 'Fizz', 'Buzz', 26, 'Fizz', 28, 29, 'Fizz',
         31, 32, 'Fizz', 34, 'Buzz', 'Fizz', 37, 38, 'Fizz', 'Buzz',
-        41, 'Fizz', 43, 44, 'Fizz', 46, 47, 48, 49, 'Buzz']
+        41, 'Fizz', 43, 44, 'Fizz', 46, 47, 'Fizz', 49, 'Buzz']
     )
   })
   test('100', () => {
     expect(fizzBuzz(100)).toEqual(
       [1, 2, 'Fizz', 4, 'Buzz', 'Fizz', 7, 8, 'Fizz', 'Buzz',
         11, 'Fizz', 13, 14, 'Fizz', 16, 17, 'Fizz', 19, 'Buzz',
-        'Fizz', 22, 23, 'Fizz', 'Buzz', 26, 27, 28, 29, 'Fizz',
+        'Fizz', 22, 23, 'Fizz', 'Buzz', 26, 'Fizz', 28, 29, 'Fizz',
         31, 32, 'Fizz', 34, 'Buzz', 'Fizz', 37, 38, 'Fizz', 'Buzz',
-        41, 'Fizz', 43, 44, 'Fizz', 46, 47, 48, 49, 'Buzz',
-        'Fizz', 52, 53, 54, 'Buzz', 56, 57, 58, 59, 'Fizz',
+        41, 'Fizz', 43, 44, 'Fizz', 46, 47, 'Fizz', 49, 'Buzz',
+        'Fizz', 52, 53, 'Fizz', 'Buzz', 56, 'Fizz', 58, 59, 'Fizz',
         61, 62, 'Fizz', 64, 'Buzz', 'Fizz', 67, 68, 'Fizz', 'Buzz',
-        71, 'Fizz', 73, 74, 'Buzz', 'Fizz', 77, 'Fizz', 79, 'Buzz',
-        81, 'Fizz', 83, 84, 'Buzz', 86, 87, 88, 89, 'Fizz',
+        71, 'Fizz', 73, 74, 'Fizz', 76, 77, 'Fizz', 79, 'Buzz',
+        'Fizz', 82, 83, 'Fizz', 'Buzz', 86, 'Fizz', 88, 89, 'Fizz',
         91, 92, 'Fizz', 94, 'Buzz', 'Fizz', 97, 98, 'Fizz', 'Buzz']
     )
   })
