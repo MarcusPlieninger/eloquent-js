529af04d1f1edd3fe1ceed353ab63a95199ddd81
MarcusPlieninger <mwp2101@columbia.edu>
2019-02-02 22:35:46 -0500

    changed == to ===

--- ch2/fizzBuzzSimple.test.js
+++ ch2/fizzBuzzSimple.test.js
@@ -2,11 +2,11 @@ function fizzBuzzSimple(size) {
   const fizzBuzzArray = []
 
   for (let i = 1; i <= size; i++) {
-    if (i % 15 == 0) {
+    if (i % 15 === 0) {
       fizzBuzzArray.push('FizzBuzz')
-    } else if (i % 3 == 0) {
+    } else if (i % 3 === 0) {
       fizzBuzzArray.push('Fizz')
-    } else if (i % 5 == 0) {
+    } else if (i % 5 === 0) {
       fizzBuzzArray.push('Buzz')
     } else {
       fizzBuzzArray.push(i)
