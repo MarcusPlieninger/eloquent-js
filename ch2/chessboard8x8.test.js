479541039c03969f43cb86cd6c0c93471f712df7
MarcusPlieninger <mwp2101@columbia.edu>
2019-02-03 12:24:55 -0500

    finalized chessboard8x8

--- ch2/chessboard8x8.test.js
+++ ch2/chessboard8x8.test.js
@@ -1,5 +1,5 @@
 function chessboard8x8() {
-  let row = ''
+  let row = '\n'
   let i
   let j
 
@@ -7,32 +7,29 @@ function chessboard8x8() {
     if (i % 2 !== 0) {
       for (j = 1; j <= 8; j++) {
         if (j % 2 !== 0) {
-          row += '#'
-        } else {
           row += ' '
-        }
-        if (row.length === 8) {
-          row += '\n'
+        } else {
+          row += '#'
         }
       }
+      row += '\n'
     } else {
       for (j = 1; j <= 8; j++) {
         if (j % 2 !== 0) {
-          row += ' '
-        } else {
           row += '#'
-        }
-        if (row.length === 8) {
-          row += '\n'
+        } else {
+          row += ' '
         }
       }
+      row += '\n'
     }
   }
+  return row
 }
 
 describe('simple', () => {
   test('8x8', () => {
-    expect(chessboard8x8()).toEqual('# # # #\n # # # #\n# # # #\n # # # #\n'
-                                    + '# # # #\n # # # #\n# # # #\n # # # #\n')
+    expect(chessboard8x8()).toEqual('\n # # # #\n# # # # \n # # # #\n# # # # \n'
+                                    + ' # # # #\n# # # # \n # # # #\n# # # # \n')
   })
 })
