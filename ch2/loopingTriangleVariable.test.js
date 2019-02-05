c96fd10d05b8efa242a49f5b77c01394b00624d7
MarcusPlieninger <mwp2101@columbia.edu>
2019-02-05 07:21:54 -0500

    renamed file and function

--- ch2/loopingTriangleVariable.test.js
+++ ch2/loopingTriangleVariable.test.js
@@ -1,4 +1,4 @@
-function loopingTriangle2(size) {
+function loopingTriangleVariable(size) {
   // input validation
   if (Number.isNaN(Number(size))) return 'C\'mon, man, just enter a number.'
   if (!size) return 'Please enter a p-o-s-i-t-v-e i-n-t-e-g-er.'
@@ -15,42 +15,42 @@ function loopingTriangle2(size) {
 
 describe('invalid inputs', () => {
   test('large negative integer', () => {
-    expect(loopingTriangle2(-230)).toBe('Triangle size must be positive.')
+    expect(loopingTriangleVariable(-230)).toBe('Triangle size must be positive.')
   })
 
   test('string', () => {
-    expect(loopingTriangle2('Dog')).toBe('C\'mon, man, just enter a number.')
+    expect(loopingTriangleVariable('Dog')).toBe('C\'mon, man, just enter a number.')
   })
 
   test('empty string', () => {
-    expect(loopingTriangle2('')).toBe('Please enter a p-o-s-i-t-v-e i-n-t-e-g-er.')
+    expect(loopingTriangleVariable('')).toBe('Please enter a p-o-s-i-t-v-e i-n-t-e-g-er.')
   })
 
   test('zero', () => {
-    expect(loopingTriangle2(0)).toBe('Please enter a p-o-s-i-t-v-e i-n-t-e-g-er.')
+    expect(loopingTriangleVariable(0)).toBe('Please enter a p-o-s-i-t-v-e i-n-t-e-g-er.')
   })
 
   test('Boolean', () => {
-    expect(loopingTriangle2(false)).toBe('Please enter a p-o-s-i-t-v-e i-n-t-e-g-er.')
+    expect(loopingTriangleVariable(false)).toBe('Please enter a p-o-s-i-t-v-e i-n-t-e-g-er.')
   })
 
   test('floating point', () => {
-    expect(loopingTriangle2(2.4325)).toBe('Triangle size cannot be floating point.')
+    expect(loopingTriangleVariable(2.4325)).toBe('Triangle size cannot be floating point.')
   })
 })
 
 describe('small inputs', () => {
   test('3', () => {
-    expect(loopingTriangle2(3)).toBe('#\n##\n###\n')
+    expect(loopingTriangleVariable(3)).toBe('#\n##\n###\n')
   })
   test('5', () => {
-    expect(loopingTriangle2(5)).toBe('#\n##\n###\n####\n#####\n')
+    expect(loopingTriangleVariable(5)).toBe('#\n##\n###\n####\n#####\n')
   })
 })
 
 describe('medium inputs', () => {
   test('10', () => {
-    expect(loopingTriangle2(10)).toBe(
+    expect(loopingTriangleVariable(10)).toBe(
       '#\n'
       + '##\n'
       + '###\n'
@@ -64,7 +64,7 @@ describe('medium inputs', () => {
     )
   })
   test('50', () => {
-    expect(loopingTriangle2(50)).toBe(
+    expect(loopingTriangleVariable(50)).toBe(
       '#\n'
       + '##\n'
       + '###\n'
