ec2c152896e66b2bb1009aec3974d064be8a42d5
MarcusPlieninger <mwp2101@columbia.edu>
2019-02-21 22:57:54 -0500

    added tests

--- ch3/countBs.test.js
+++ ch3/countBs.test.js
@@ -7,3 +7,25 @@ function countBs(theString) {
   }
   return counter
 }
+
+describe('simple inputs', () => {
+  test('5 B\'s in a row', () => {
+    expect(countBs('BBBBB')).toBe(5)
+  })
+
+  test('5 b\'s in a row', () => {
+    expect(countBs('bbbbb')).toBe(0)
+  })
+
+  test('Banana', () => {
+    expect(countBs('Banana')).toBe(1)
+  })
+
+  test('BuBBa', () => {
+    expect(countBs('BuBBa')).toBe(3)
+  })
+
+  test('Boolean', () => {
+    expect(countBs('Boolean')).toBe(1)
+  })
+})
