42a2176da63ccf8eae0345392b3654126a4e9c2d
MarcusPlieninger <mwp2101@columbia.edu>
2019-02-23 14:32:08 -0500

    fixed tests

--- ch3/isEven.test.js
+++ ch3/isEven.test.js
@@ -1,29 +1,52 @@
-function isEven(theNumber) {
-  // draft solution
-  if (theNumber === 0) return true
-  if (theNumber === 1) return false
-  isEven(theNumber - 2)
+function isEven(aNumber) {
+  // input validation - I explain why the order of the if statements matters here
+  // first tests for array since isNaN would be positive for an array as well
+  if (Array.isArray(aNumber)) return 'Input cannot be an array.'
+  // then tests for values that are or coerce to false (eg, false, empty string, so that their
+  // negation makes them true, provided that the inputs are not 0, since 0 is a valid input
+  if ((!aNumber) && (aNumber !== 0)) return 'Please enter a positive or negative integer.'
+  // then tests for NaN
+  if (Number.isNaN(Number(aNumber))) return 'Please, only a positive or negative integer.'
+  // then tests for either of the inputs being the Boolean primitive value of true
+  if (aNumber === true) return 'True is not a valid input. Please enter a positive or negative integer.'
+  // finally tests for floating point
+  if (aNumber % 1 !== 0) {
+    return 'Input cannot be floating point. Please enter only a postiive or negative integer.'
+  }
+
+  if (aNumber === 0) {
+    return true
+  }
+  if (aNumber === 1) {
+    return false
+  }
+  if (aNumber > 0) {
+    return isEven(aNumber - 2)
+  }
+  if (aNumber < 0) {
+    return isEven(aNumber + 2)
+  }
 }
-// draft tests
+
 describe('invalid inputs', () => {
   test('string', () => {
-    expect(isEven('Dog')).toBe('Please enter a positive or negative integer.')
+    expect(isEven('Dog')).toBe('Please, only a positive or negative integer.')
   })
-
   test('empty string', () => {
     expect(isEven('')).toBe('Please enter a positive or negative integer.')
   })
-
-  test('Boolean', () => {
+  test('Boolean: false', () => {
     expect(isEven(false)).toBe('Please enter a positive or negative integer.')
   })
-
+  test('Boolean: true', () => {
+    expect(isEven(true)).toBe('True is not a valid input. Please enter a positive or negative integer.')
+  })
   test('Array', () => {
     expect(isEven([1, 2, 3, 4, 5])).toBe('Input cannot be an array.')
   })
 
   test('floating point', () => {
-    expect(isEven(1.432534, 2)).toBe('Input cannot be floating point.')
+    expect(isEven(1.432534)).toBe('Input cannot be floating point. Please enter only a postiive or negative integer.')
   })
 })
 
@@ -38,7 +61,7 @@ describe('small inputs', () => {
     expect(isEven(2)).toBe(true)
   })
   test('odd', () => {
-    expect(isEven(3)).toBe(true)
+    expect(isEven(3)).toBe(false)
   })
 })
 
@@ -47,15 +70,15 @@ describe('medium inputs', () => {
     expect(isEven(50)).toBe(true)
   })
   test('medium negative', () => {
-    expect(isEven(101)).toBe(false)
+    expect(isEven(75)).toBe(false)
   })
 })
 
 describe('large inputs', () => {
   test('large positive', () => {
-    expect(isEven(1333, -4000)).toBe(-4000)
+    expect(isEven(500)).toBe(true)
   })
   test('large negative', () => {
-    expect(isEven(-243609348, 20)).toBe(-243609348)
+    expect(isEven(-331)).toBe(false)
   })
 })
