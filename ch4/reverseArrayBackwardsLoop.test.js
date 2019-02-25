f4401283af8e1ff27cf6b6bf8b92a827c84ae00e
MarcusPlieninger <mwp2101@columbia.edu>
2019-02-25 16:58:29 -0500

    added tests, fixed typos

--- ch4/reverseArrayBackwardsLoop.test.js
+++ ch4/reverseArrayBackwardsLoop.test.js
@@ -6,8 +6,13 @@ function reverseArrayBackwardsLoop(arrayInput) {
   return newArray
 }
 
-describe('test taken from text', () => {
+describe('tests taken from text', () => {
   test('["A", "B", "C"]', () => {
     expect(reverseArrayBackwardsLoop(['A', 'B', 'C'])).toEqual(['C', 'B', 'A'])
+  })
+  test('[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]', () => {
+    expect(reverseArrayBackwardsLoop([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])).toEqual(
+      [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+    )
   })
 })
--- ch4/reverseArrayUnshift.test.js
+++ ch4/reverseArrayUnshift.test.js
@@ -12,6 +12,7 @@ describe('tests taken from text', () => {
   })
   test('[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]', () => {
     expect(reverseArrayUnshift([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])).toEqual(
-      [10, 9, 8, 7, 6, 5, 4, 3, 2, 1])
+      [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+    )
   })
 })
