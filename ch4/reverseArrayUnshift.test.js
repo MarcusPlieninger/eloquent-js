640a0ad05f45e13a681d17b750a340564c0f3e96
MarcusPlieninger <mwp2101@columbia.edu>
2019-02-25 16:54:46 -0500

    added test

--- ch4/reverseArrayUnshift.test.js
+++ ch4/reverseArrayUnshift.test.js
@@ -10,4 +10,8 @@ describe('test taken from text', () => {
   test('["A", "B", "C"]', () => {
     expect(reverseArrayUnshift(['A', 'B', 'C'])).toEqual(['C', 'B', 'A'])
   })
+  test('[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]', () => {
+    expect(reverseArrayUnshift([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])).toEqual(
+      [10, 9, 8, 7, 6, 5, 4, 3, 2, 1])
+  })
 })
