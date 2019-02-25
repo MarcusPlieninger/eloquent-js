59ae9e095db67f4f05312b4bfb12168e2a82f0a1
MarcusPlieninger <mwp2101@columbia.edu>
2019-02-25 16:25:38 -0500

    renamed file, added test

--- ch4/reverseArray.test.js
+++ ch4/reverseArray.test.js
@@ -1,13 +1,13 @@
-function reverseArray(arrayInput) {
+function reverseArrayUnshift(arrayInput) {
   const newArray = []
   for (const i of arrayInput) {
-    newArray.push(arrayInput.pop(i))
+    newArray.unshift(i)
   }
   return newArray
 }
 
 describe('test taken from text', () => {
   test('["A", "B", "C"]', () => {
-    expect(reverseArray(['A', 'B', 'C'])).toBe(['C', 'B', 'A'])
+    expect(reverseArrayUnshift(['A', 'B', 'C'])).toEqual(['C', 'B', 'A'])
   })
 })
