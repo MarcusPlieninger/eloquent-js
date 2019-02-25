0195ed0fc6b64abdd5be4847c7a9d19ff2c1658c
MarcusPlieninger <mwp2101@columbia.edu>
2019-02-25 15:44:53 -0500

    added tests

--- ch4/rangeModified.test.js
+++ ch4/rangeModified.test.js
@@ -1,13 +1,21 @@
 function rangeModified(start, end, step = 1) {
-  const rangeArray = []
+  const result = []
   if (start < end) {
     for (let i = start; i <= end; i += step) {
-      rangeArray.push(i)
+      result.push(i)
     }
-  } else if (start > end) {
-    for (let i = start; i >= end; i -= step) {
-      rangeArray.push(i)
-    }
+    return result
   }
-  return rangeArray
+  for (let i = start; i >= end; i -= -step) {
+    result.push(i)
+  }
+  return result
 }
+describe('tests taken from text', () => {
+  test('1 to 10, default increment', () => {
+    expect(rangeModified(1, 10)).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+  })
+  test('5 to 2, decrement of 1', () => {
+    expect(rangeModified(5, 2, -1)).toEqual([5, 4, 3, 2])
+  })
+})
