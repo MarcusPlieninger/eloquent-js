cc7122dec83073afb0ca3f5f52d3ca8b2d4f4c5f
MarcusPlieninger <mwp2101@columbia.edu>
2019-03-05 06:47:13 -0500

    added 3 test, failed

--- ch4/arrayToList.test.js
+++ ch4/arrayToList.test.js
@@ -1,9 +1,11 @@
 function arrayToList(arrayInput) {
-  if (arrayInput.length === 0) return {}
+  const list = {}
+  let rest = null
 
-  const list = { rest: null }
-  for (const entry of arrayInput) {
-    list.value = entry
+  for (let i = list.length; i > 0; i--) {
+    list.rest = rest
+    list.value = arrayInput[i - 1]
+    rest = Object.assign({}, list)
   }
   return list
 }
@@ -13,6 +15,20 @@ describe('arrayToList()', () => {
     expect(arrayToList([])).toEqual({})
   })
   test('array with 1 value', () => {
-    expect(arrayToList([3])).toEqual({ rest: null, value: 3 })
+    expect(arrayToList([3])).toEqual({ value: 3, rest: null })
+  })
+  test('array with 3 values', () => {
+    expect(arrayToList([1, 3, 5])).toEqual(
+      {
+        value : 1,
+        rest  : {
+          value : 3,
+          rest  : {
+            value : 5,
+            rest  : null
+          }
+        }
+      }
+    )
   })
 })
