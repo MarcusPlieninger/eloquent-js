24f671a814e9e7f4e629fdb54614e593ab524804
MarcusPlieninger <mwp2101@columbia.edu>
2019-03-06 11:49:53 -0500

    added tests for empty list and list with 1 value, passed

---
+++ ch4/listToArray.test.js
@@ -0,0 +1,31 @@
+function listToArray(listInput) {
+  const arrayOutput = []
+  while (listInput.value !== undefined) {
+    arrayOutput.push(listInput.value)
+    delete listInput.value
+  }
+  return arrayOutput
+}
+
+describe('listToArray()', () => {
+  test('empty list', () => {
+    expect(listToArray({})).toEqual([])
+  })
+  test('list with 1 value', () => {
+    expect(listToArray({ value: 3, rest: null })).toEqual([3])
+  })/*
+  test('list with 3 values', () => {
+    expect(listToArray([1, 3, 5])).toEqual(
+      {
+        value : 1,
+        rest  : {
+          value : 3,
+          rest  : {
+            value : 5,
+            rest  : null
+          }
+        }
+      }
+    )
+  }) */
+})
