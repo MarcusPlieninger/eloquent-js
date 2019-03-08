c58b06f2f59ffb1eb0b385b9709c852bd8b10fc8
MarcusPlieninger <mwp2101@columbia.edu>
2019-03-07 22:12:27 -0500

    renamed file and function

--- ch4/listToArrayWhileLoop.test.js
+++ ch4/listToArrayWhileLoop.test.js
@@ -1,4 +1,4 @@
-function listToArray(inputList) {
+function listToArrayWhileLoop(inputList) {
   const outputArray = []
   while ('value' in inputList) {
     outputArray.push(inputList.value)
@@ -7,15 +7,15 @@ function listToArray(inputList) {
   return outputArray
 }
 
-describe('listToArray()', () => {
+describe('listToArrayWhileLoop()', () => {
   test('empty list', () => {
-    expect(listToArray({})).toEqual([])
+    expect(listToArrayWhileLoop({})).toEqual([])
   })
   test('list with 1 value', () => {
-    expect(listToArray({ value: 3, rest: null })).toEqual([3])
+    expect(listToArrayWhileLoop({ value: 3, rest: null })).toEqual([3])
   })
   test('list with 3 values', () => {
-    expect(listToArray({ value: 1, rest: { value: 3, rest: { value: 5, rest: null } } })).toEqual(
+    expect(listToArrayWhileLoop({ value: 1, rest: { value: 3, rest: { value: 5, rest: null } } })).toEqual(
       [1, 3, 5]
     )
   })
