bb94f547dc746fbb88eb0eee31a2a3e3a3e1210f
MarcusPlieninger <mwp2101@columbia.edu>
2019-03-11 23:03:18 -0400

    added missing return, improved first if statement, tests passed

--- ch4/nthPositionRecursive.test.js
+++ ch4/nthPositionRecursive.test.js
@@ -1,23 +1,7 @@
 function nthPositionRecursive(inputList, position) {
+  if (!inputList) return undefined
   if (position === 0) return inputList.value
-  if (inputList.rest === undefined) return undefined
-  nthPositionRecursive(inputList.rest, position--)
-}
-
-// easier way is to convert list to array and then return the index of the array
-function nthPostionRecursiveEasy(inputList, position) {
-  const inputListAsArray = listToArrayRecursive(inputList)
-  // indexOf returns -1 if the element is not in the array, hence this conditional statement
-  if (position <= inputListAsArray.length && position >= 0) return inputListAsArray[position]
-  return undefined
-}
-
-function listToArrayForLoop(inputList) {
-  const outputArray = []
-  for (let node = inputList; node; node = node.rest) {
-    outputArray.push(node.value)
-  }
-  return outputArray
+  return nthPositionRecursive(inputList.rest, position - 1)
 }
 
 describe('nthPostionRecursive()', () => {
@@ -39,41 +23,6 @@ describe('nthPostionRecursive()', () => {
   })
   test('3-element list, position not in range', () => {
     expect(nthPositionRecursive(
-      {
-        value : 1,
-        rest  :
-         {
-           value : 3,
-           rest  :
-           {
-             value : 5,
-             rest  : null
-           }
-         }
-      }, 7
-    )).toBe(undefined)
-  })
-})
-
-describe('nthPostionRecursiveEasy()', () => {
-  test('3-element list, property in list', () => {
-    expect(nthPostionRecursiveEasy(
-      {
-        value : 1,
-        rest  :
-         {
-           value : 3,
-           rest  :
-           {
-             value : 5,
-             rest  : null
-           }
-         }
-      }, 2
-    )).toBe(5)
-  })
-  test('3-element list, position not in range', () => {
-    expect(nthPostionRecursiveEasy(
       {
         value : 1,
         rest  :
