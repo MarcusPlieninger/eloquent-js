c9f402e38d94b5734a87c9fa7bfcc300aba20360
MarcusPlieninger <mwp2101@columbia.edu>
2019-03-13 11:39:20 -0400

    added tests for inputs that are not objects, passed

--- ch4/deepEqual.test.js
+++ ch4/deepEqual.test.js
@@ -1,11 +1,32 @@
-function deepEqual(A, B) {
-
+function deepEqual(obj1, obj2) {
+  if (typeof (obj1) !== 'object' || typeof (obj1) !== 'object' || obj1 === null || obj2 === null) {
+    return obj1 === obj2
+  }
 }
 
-describe('deepEqual() with no objects', => {
-
-})
-
-describe('deepEqual()', => {
-    test('identity')
+describe('inputs that are not objects', () => {
+  test('Boolean', () => {
+    expect(deepEqual(true, false)).toBe(false)
+    expect(deepEqual(true, 3)).toBe(false)
+  })
+  test('null', () => {
+    expect(deepEqual(null, null)).toBe(true)
+    expect(deepEqual(null, 3)).toBe(false)
+  })
+  test('undefined', () => {
+    expect(deepEqual(undefined, undefined)).toBe(true)
+    expect(deepEqual(undefined, 'cat')).toBe(false)
+  })
+  test('number', () => {
+    expect(deepEqual(3, 4)).toBe(false)
+    expect(deepEqual(5, 5)).toBe(true)
+  })
+  test('string', () => {
+    expect(deepEqual('cat', 'dog')).toBe(false)
+    expect(deepEqual('cat', 'cat')).toBe(true)
+  })
+/*   test('symbol', () => {
+    expect(deepEqual( , ).toBe()
+    expect(deepEqual( , ).toBe()
+  }) */
 })
