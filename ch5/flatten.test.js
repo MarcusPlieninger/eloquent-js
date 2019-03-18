fd1536f4cffd54eb5fa447cc0c4af5d284e40549
MarcusPlieninger <mwp2101@columbia.edu>
2019-03-18 15:50:14 -0400

    added note about pure function as well as console.log statements

--- ch5/flatten.test.js
+++ ch5/flatten.test.js
@@ -1,14 +1,17 @@
 function flatten(arrays) {
+// reduce() results in a pure function
   return arrays.reduce((a, b) => a.concat(b))
 }
 
 describe('flatten()', () => {
+  const arrays1 = [[1, 2, 3], [4, 5], [6]]
   test('example from text', () => {
-    expect(flatten([[1, 2, 3], [4, 5], [6]])).toEqual([1, 2, 3, 4, 5, 6])
+    expect(flatten(arrays1)).toEqual([1, 2, 3, 4, 5, 6])
+    console.log(arrays1) // proves original array is in tact
   })
   test('longer array', () => {
-    expect(flatten([[1, 2, 3, 4, 5], [6, 7, 8, 9], [10, 11, 12], [13, 14], [15]])).toEqual(
-      [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
-    )
+    const arrays2 = [[1, 2, 3, 4, 5], [6, 7, 8, 9], [10, 11, 12], [13, 14], [15]]
+    expect(flatten(arrays2)).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15])
+    console.log(arrays2) // proves original array is in tact
   })
 })
