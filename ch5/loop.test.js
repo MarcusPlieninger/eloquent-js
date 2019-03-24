61b6db116657e56939bb671e276088a653f6d96a
MarcusPlieninger <mwp2101@columbia.edu>
2019-03-24 11:57:33 -0400

    working on loopFor

--- ch5/loop.test.js
+++ ch5/loop.test.js
@@ -1,17 +1,44 @@
-function loop(value, testF, updateF, bodyF) {
-  const result = []
-  let counter = value
-  while (testF(counter)) {
-    bodyF
-    counter = updateF
+/* function loop(value, test, update, body) {
+  while (test(value)) {
+    body(value)
+    value = update(value)
   }
+}
 
+loop(3, n => n > 0, n => n - 1, console.log);
+// → 3
+// → 2
+// → 1 */
+
+
+function loopWhile(n, test, update, body) {
+  const result = []
+  while (test(n)) {
+    result.push(body(n))
+    n = update(n)
+  }
   return result
 }
 
-describe('loop()', () => {
+function loopFor(n, test, update, body) {
+  const result = []
+  for (n; test(n); update(n)) {
+    result.push(body(n))
+  }
+  return result
+}
+
+describe('loopWhile()', () => {
   test('example from text', () => {
-    const anArray = []
-    expect(loop(3, n => n > 0, n => n - 1, n => anArray.push(n)).toEqual([3, 2, 1]))
+    expect(loopWhile(3, n => n > 0, n => n - 1, n => String(n))).toEqual(['3', '2', '1'])
   })
 })
+
+describe('loopFor()', () => {
+  test('example from text', () => {
+    expect(loopFor(3, n => n > 0, n => n - 1, n => String(n))).toEqual(['3', '2', '1'])
+  })
+})
+
+// Note: Instead of console.log which does not return a value,
+// the value is converted to a string and returned in an array
