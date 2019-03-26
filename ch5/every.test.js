399e6ec9c07f40afb66ed9c0cb10a39fff42dea1
MarcusPlieninger <mwp2101@columbia.edu>
2019-03-26 15:41:21 -0400

    tests passed, remaining task: -add explanation of how .some() variation makes use of De Morgan's laws

--- ch5/every.test.js
+++ ch5/every.test.js
@@ -1,12 +1,28 @@
+function everyLoop(anArray, aPredicate) {
+  for (const n of anArray) {
+    if (aPredicate(n) === false) return false
+  }
+  return true
+}
 
-
-
-
+function everySome(anArray, aPredicate) {
+  return (!anArray.some(n => !aPredicate(n)))
+}
 
 describe('everyLoop()', () => {
-    test('examples from text', () => {
-      expect(every([1, 3, 5], n => n < 10)).toEqual(['3', '2', '1'])
-      expect(every([2, 4, 16], n => n < 10)).to
-  
-
+  test('examples from text', () => {
+    expect(everyLoop([1, 3, 5], n => n < 10)).toEqual(true)
+    expect(everyLoop([2, 4, 16], n => n < 10)).toEqual(false)
+    expect(everyLoop([], n => n < 10)).toEqual(true)
   })
+})
+
+describe('everySome()', () => {
+  test('examples from text', () => {
+    expect(everySome([1, 3, 5], n => n < 10)).toEqual(true)
+    expect(everySome([2, 4, 16], n => n < 10)).toEqual(false)
+    expect(everySome([], n => n < 10)).toEqual(true)
+  })
+})
+
+/* Add explanation here for how the .some() method variation makes use of deMorgan's laws.
