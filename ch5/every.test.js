05a40152010fa758729c8f0da42a02159527dd9d
MarcusPlieninger <mwp2101@columbia.edu>
2019-03-25 22:03:50 -0400

    started writing tests

---
+++ ch5/every.test.js
@@ -0,0 +1,12 @@
+
+
+
+
+
+describe('everyLoop()', () => {
+    test('examples from text', () => {
+      expect(every([1, 3, 5], n => n < 10)).toEqual(['3', '2', '1'])
+      expect(every([2, 4, 16], n => n < 10)).to
+  
+
+  })
